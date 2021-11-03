1ebb0a18af56ac6c3e9bd902ae3d4f77f5ac0cf3
muirisc <91299876+muirisc@users.noreply.github.com>
2021-11-03 12:35:21 -0400

    commit tree 8d6ad28dd66712cfa6949dffb370792e18252326

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+function findMatching(array, name) {
+const result = []
+for (const driver of array)
+    if (driver.toLowerCase() == name.toLowerCase()){
+        result.push(driver)
+    }
+    return result 
+}
+
+function fuzzyMatch(array, letter){
+const result2 = []
+for (const driver of array)
+    if (driver.charAt(0) == letter.charAt(0)){
+        result2.push(driver)
+    }
+    return result2
+}
+
+function matchName(array, string){
+const result3 = []
+for (const driver of array)
+    if (driver.name == string){
+    
+        result3.push(driver)
+}
+    return result3
+}
